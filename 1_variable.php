c27f590fe5d0a59edfd85f6e94bbcb4efd5c47aa
Uchchash346 <mru416@gmail.com>
2022-09-20 11:30:25 +0600

    Variables

--- 1_variable.php
+++ 1_variable.php
@@ -8,9 +8,9 @@ echo "<br>";
 echo $x;
 echo "<br>";
 echo $y;
-?>
-
 /*
 --------- PHP is a Loosely Typed Language ----------
 A loosely typed language is a language that does not require the data type of a variable. In PHP, all you have to do start with a dollar sign ($). You do not have to define the data type.
 */
+
+?>
