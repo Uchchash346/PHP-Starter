a1db18d34396d4cb254833761c05607383145f90
Uchchash346 <mru416@gmail.com>
2022-09-20 11:47:43 +0600

    echo and print

--- 2_echo_print.php
+++ 2_echo_print.php
@@ -2,6 +2,14 @@
 // echo can not return value but print can.
 // echo can take multiple parameters but print can take one argument
 // echo is faster than print
+
+
+// 1. echo statement
 echo "<h2>It's me Mahmudur Rahman.</h2>";
 echo "I ", "am ", "from ", "SHYAMOLI";
+
+echo "<br>";
+
+// 2. print statement
+print  "<h2>This is a print statement.</h2>"
 ?>
