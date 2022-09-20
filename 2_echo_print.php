282cf8553414d042d2bde0cb886ef4ed1ac66666
Uchchash346 <mru416@gmail.com>
2022-09-20 11:45:45 +0600

    echo and print

---
+++ 2_echo_print.php
@@ -0,0 +1,7 @@
+<?php
+// echo can not return value but print can.
+// echo can take multiple parameters but print can take one argument
+// echo is faster than print
+echo "<h2>It's me Mahmudur Rahman.</h2>";
+echo "I ", "am ", "from ", "SHYAMOLI";
+?>
