a7dcca8839043614352f8e9e1cec6b5fe4bdccce
Uchchash346 <mru416@gmail.com>
2022-09-20 13:23:21 +0600

    loops

---
+++ 9_loops.php
@@ -0,0 +1,17 @@
+<?php
+// While loop
+$x = 1;
+while($x <= 5){
+    echo "The number is $x <br>";
+    $x++;
+}
+
+echo "<br>";
+
+// for loop
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+  }
+  
+
+?>
