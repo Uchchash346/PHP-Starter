ff01a5c46b0937b5cba36d9bdec549a7ef4c1845
Uchchash346 <mru416@gmail.com>
2022-09-20 12:00:53 +0600

    data types

---
+++ 3_data_types.php
@@ -0,0 +1,26 @@
+<?php
+/*
+There are various data types in php. These are:
+    1. String
+    2. Integer
+    3. Float
+    4. Boolean
+    5. Array
+    6. Object
+    7. NULL
+    8. Resource
+*/
+
+$x = "Mahmudur";
+$y = 'Rahman';
+
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+
+// Get data type by using var_dump()
+$id = 11276;
+var_dump($id);
+
+?>
